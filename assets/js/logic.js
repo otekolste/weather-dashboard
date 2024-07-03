b01332f2af94a54a46bc2c2ba1519638fc0f094c
otekolste <58699635+otekolste@users.noreply.github.com>
2024-07-03 17:20:17 -0400

    implemented html rendering for forecast info

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,14 @@
 const APIKey = "50ed25e94a50cae769760c988183a9a5";
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid={API key};
+let weatherArray = [];
+let dailyArray = [];
 
 function handleSearch(event) {
 
     event.preventDefault();
 
     let cityName = $("#cityName").val();
-    //console.log(cityName);
     getWeatherInfo(cityName);
 
 }
@@ -33,7 +34,7 @@ function getWeatherInfo(cityName) {
         console.log(result);
         let lat = result[0];
         let lon = result[1];
-        const newApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+        const newApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
         return fetch(newApiUrl);
     })
     .then(function(response) {
@@ -45,48 +46,104 @@ function getWeatherInfo(cityName) {
             console.log("NO");
         }
     })
+    .then(function(result) {
+        console.log(result);
+        weatherArray = result;
+
+        renderWeatherInfo();
+
+    })
+    /*
     .then(function (result){
         console.log(result);
-    })
+      //  weatherArray = result;
+       // renderWeatherInfo();
+       let groupedArray = [];
 
-    /*
+       let breakpoint = result["list"].findIndex((element) => element.dt_txt.substring(11) == "00:00:00");
+     //  console.log(breakpoint);
+        let weatherObj = {
+            temp:0,
+            wind:0,
+            humidity:0,
 
-    fetch(apiUrl)
-    .then (function(response) {
-        let coords = [];
-        if(response.ok) {
-            response.json().then(function (data) {
-                coords = [data[0].lat,data[0].lon];
-                return coords;
-            })
-            .then(function(result) {
-                lat = result[0];
-                lon = result[1];
-                const newApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`
-                fetch(newApiUrl)
-                .then(function(response) {
-                    if(response.ok) {
-                        response.json().then(function (data) {
-                            console.log(data);
-                        })
-            
-                    }
-                    else{
-                        console.log("NO!!!");
-                        return;
-                    }
-                })
-            })
-        }
-        else {
-            // TODO: handle bad fetch
-            console.log("AAAA");
-            return;
         }
 
-    })
+        for(let i = 0; i<breakpoint;i++) {
 
-    */
+        }
+
+
+
+
+
+
+
+    })
+        */
+
+}
+
+function pickIcon(condition) {
+    if(condition == 800) {
+        return '<i class="fa-regular fa-sun"></i>';
+    }
+    else if(condition > 800) {
+        return '<i class="fa-solid fa-cloud"></i>';
+    }
+    else if(condition > 700) {
+        return '<i class="fa-solid fa-smog"></i>';
+    }
+    else if(condition >= 600) {
+        return '<i class="fa-solid fa-snowflake"></i>';
+    }
+    else if(condition >= 300) {
+        return '<i class="fa-solid fa-cloud-rain"></i>';
+    }
+    else if(condition>= 200) {
+        return '<i class="fa-solid fa-cloud-bolt"></i>';
+    }
+    else{
+        return 'Error rendering icon';
+    }
+
+
+}
+
+
+
+function renderWeatherInfo() {
+    $('#currentDisplay').append(`
+        <h3>${weatherArray["city"].name} (${dayjs.unix(weatherArray["list"][0].dt).format("MM/D/YYYY")})</h3>
+        <ul class="list-unstyled fs-5">
+            <li>Temp: ${weatherArray["list"][0]["main"].temp}</li>
+            <li>Wind: ${weatherArray["list"][0]["wind"].speed} MPH</li>
+            <li>Humidity: ${weatherArray["list"][0]["main"].humidity}%</li>
+
+        </ul>
+        `
+    );
+   // let breakpoint = weatherArray["list"].findIndex((element) => element.dt_txt.substring(11) == "00:00:00");
+   // console.log(breakpoint);
+
+    for(let i = 7; i<weatherArray["list"].length;i+=8) {
+        console.log(weatherArray["list"][i]["weather"][0].id);
+      //  console.log(weatherArray["list"][i]);
+        $('#futureDisplay').append(`
+            <div class="card col text-light text-center" style="background-color: #16223F">
+                <div class="card-body">
+                    <h4>${dayjs.unix(weatherArray["list"][i].dt).format("MM/D/YYYY")}</h4>
+                    ${pickIcon(weatherArray["list"][i]["weather"][0].id)}
+                    <ul class="list-unstyled fs-5">
+                        <li>Temp: ${weatherArray["list"][i]["main"].temp}</li>
+                        <li>Wind: ${weatherArray["list"][i]["wind"].speed} MPH</li>
+                        <li>Humidity: ${weatherArray["list"][i]["main"].humidity}%</li>
+                    </ul>
+                </div> 
+            </div>
+        `);
+
+    }
 
 }
 
