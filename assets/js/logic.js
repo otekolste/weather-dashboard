c2abae33be88738344e872fdd376c3720c954d84
otekolste <58699635+otekolste@users.noreply.github.com>
2024-07-09 16:44:27 -0400

    added getAverage functions for temp, humidity, and wind

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,7 @@
 const APIKey = "50ed25e94a50cae769760c988183a9a5";
 
+
+
 // http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid={API key};
 let citiesList = JSON.parse(localStorage.getItem("citiesList"));
 
@@ -38,17 +40,20 @@ function getWeatherInfo(cityName) {
     .then(function(result) {
         let lat = result[0];
         let lon = result[1];
+        const newApiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
         const newApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
-        return fetch(newApiUrl);
+        const promises = [newApiUrlCurrent, newApiUrl].map(url => fetch(url));
+        return Promise.all(promises);
     })
     .then(function(response) {
-        return response.json();
+        return Promise.all(response.map(response => response.json()));
     })
-    .then(function(result) {
-        weatherArray = result;
+    .then(function(response) {
+        weatherArray = response;
+     //   console.log(weatherArray);
 
-        if(!searchedCities.includes(weatherArray["city"].name)) {
-            searchedCities.push(weatherArray["city"].name);
+        if(!searchedCities.includes(weatherArray[0].name)) {
+            searchedCities.push(weatherArray[0].name);
             localStorage.setItem("citiesList", JSON.stringify(searchedCities));
         }
 
@@ -57,7 +62,7 @@ function getWeatherInfo(cityName) {
 
     })
     .catch((error) =>{
-        console.log("Error!");
+        console.log("Error: ", error);
         renderErrorMessage();
     })
 
@@ -110,20 +115,84 @@ function pickIcon(condition) {
 
 
 
+function getAverageTemp(start, stop){
+    if(stop>40) {
+        stop = 40;
+    }
+    let temp = 0;
+    for(let i = start; i<stop; i++) {
+        temp+=weatherArray[1]["list"][start]["main"].temp;
+    }
+    temp/=(stop-start);
+
+    return Math.round(temp * 100) / 100;
+
+}
+function getAverageWind(start, stop){
+    if(stop>40) {
+        stop = 40;
+    }
+    let wind = 0;
+    for(let i = start; i<stop; i++) {
+        wind+=weatherArray[1]["list"][start]["wind"].speed;
+    }
+    wind/=(stop-start);
+
+    return Math.round(wind * 100) / 100;
+
+}
+function getAverageHum(start, stop){
+    if(stop>40) {
+        stop = 40;
+    }
+    let hum = 0;
+    for(let i = start; i<stop; i++) {
+        hum+=weatherArray[1]["list"][start]["main"].humidity;
+    }
+    hum/=(stop-start);
+
+    return Math.round(hum * 100) / 100;
+
+}
+
+
+
 function renderWeatherInfo() {
     $('#currentDisplay').html(`
-        <h3 class="mt-2">${weatherArray["city"].name} (${dayjs.unix(weatherArray["list"][0].dt).format("MM/D/YYYY")}) ${pickIcon(weatherArray["list"][0]["weather"][0].id)}
+        <h3 class="mt-2">${weatherArray[0].name} (${dayjs.unix(weatherArray[0].dt).format("MM/D/YYYY")}) ${pickIcon(weatherArray[0]["weather"][0].id)}
 </h3>
         <ul class="list-unstyled fs-5">
-            <li class="my-2">Temp: ${weatherArray["list"][0]["main"].temp}</li>
-            <li class="my-2">Wind: ${weatherArray["list"][0]["wind"].speed} MPH</li>
-            <li class="my-2">Humidity: ${weatherArray["list"][0]["main"].humidity}%</li>
+            <li class="my-2">Temp: ${weatherArray[0]["main"].temp}</li>
+            <li class="my-2">Wind: ${weatherArray[0]["wind"].speed} MPH</li>
+            <li class="my-2">Humidity: ${weatherArray[0]["main"].humidity}%</li>
 
         </ul>
         `
     );
-//    let breakpoint = weatherArray["list"].findIndex((element) => element.dt_txt.substring(11) == "00:00:00");
 
+    let breakpoint = weatherArray[1]["list"].findIndex((element) => element.dt_txt.substring(11) == "00:00:00");
+//    console.log(breakpoint);
+    $('#futureDisplay').html('');
+    console.log(weatherArray);
+    for(let i = 0; i<5;i++) {
+        console.log(getAverageTemp(breakpoint,breakpoint+8));
+        $('#futureDisplay').append(`
+            <div class="card mx-1 col-md-2 col-sm-6 text-light text-center" style="background-color: #16223F">
+                <div class="card-body">
+                    <h4>${weatherArray[1]["list"][breakpoint].dt_txt.substring(0,11)}</h4>
+                    <ul class="list-unstyled fs-5">
+                        <li class="my-2">Temp: ${getAverageTemp(breakpoint, breakpoint+8)}</li>
+                        <li class="my-2">Wind: ${getAverageWind(breakpoint, breakpoint+8)} MPH</li>
+                        <li class="my-2">Humidity: ${getAverageHum(breakpoint, breakpoint+8)}%</li>                        
+                    </ul>
+                </div> 
+            </div>`);
+        breakpoint+=8;
+
+
+    }
+//    let breakpoint = weatherArray["list"].findIndex((element) => element.dt_txt.substring(11) == "00:00:00");
+/*
    $('#futureDisplay').html('');
 
     for(let i = 7; i< weatherArray["list"].length; i+=9) {
@@ -143,6 +212,7 @@ function renderWeatherInfo() {
         `);
 
     }
+        */
 
 }
 
